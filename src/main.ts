cebc739ca78560f5336be13d9013d0cdfc9d59c3
Kristián Duda <kristian.duda@extropy.sk>
2023-06-21 13:53:53 +0200

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import * as cookieParser from "cookie-parser";
+import { SESSION_COOKIE_NAME } from "src/auth/guards/session.guard";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
@@ -17,6 +18,7 @@ async function bootstrap() {
     .setTitle("Passwordless")
     .setDescription("Passwordless API")
     .setVersion(process.env.npm_package_version)
+    .addCookieAuth(SESSION_COOKIE_NAME)
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
